5390d424815e50cac9d9df85169fa567b5f16db1
hamzahajyousef97 <hamzahajyousef@gmail.com>
2023-11-10 13:50:09 +0300

    qwdlq

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const whitelist = ["http://localhost:5050", "http://localhost:4200", "http://localhost:4300", "http://localhost:4400", "truted-hosting.web.app"];
+const whitelist = [
+     "http://localhost:5050",
+     "http://localhost:4200",
+     "http://localhost:4300",
+     "http://localhost:4400",
+     "https://truted-hosting.web.app",
+];
 var corsOptionsDelegate = (req, callback) => {
      var corsOptions;
 
